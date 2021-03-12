2c0954ad99117f9ca062b4dc6637b266e056d5d1
Jen Lampton <jen@jeneration.com>
2021-03-11 21:41:26 -0800

    more rows on the schedule

--- www/themes/borg_events/templates/views-view-grid.tpl.php
+++ www/themes/borg_events/templates/views-view-grid.tpl.php
@@ -5,36 +5,38 @@
  * - overrides H3 for group heading.
  */
 ?>
-<?php if (!empty($title)) : ?>
-  <div class="group-head"><?php print $title; ?></div>
-<?php endif; ?>
-
-<?php if (empty($deprecated_table)) : ?>
-<div class="<?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
-  <?php foreach ($rows as $row_count => $row): ?>
-    <div <?php if (!empty($row_classes[$row_count])) { print 'class="' . implode(' ', $row_classes[$row_count]) . '"';  } ?>>
-      <?php print $row; ?>
-    </div>
-  <?php endforeach; ?>
-</div>
-<?php endif; ?>
-
-<?php if (!empty($deprecated_table)) : ?>
-<table class="<?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
-  <?php if (!empty($caption)) : ?>
-    <caption><?php print $caption; ?></caption>
+<div class="group">
+  <?php if (!empty($title)) : ?>
+    <div class="group-head"><?php print $title; ?></div>
   <?php endif; ?>
 
-  <tbody>
-    <?php foreach ($rows as $row_number => $columns): ?>
-      <tr <?php if (!empty($row_classes[$row_number])) { print 'class="' . implode(' ', $row_classes[$row_number]) .'"';  } ?>>
-        <?php foreach ($columns as $column_number => $item): ?>
-          <td <?php if ($column_classes[$row_number][$column_number]) { print 'class="' . implode(' ', $column_classes[$row_number][$column_number]) .'"';  } ?>>
-            <?php print $item; ?>
-          </td>
-        <?php endforeach; ?>
-      </tr>
+  <?php if (empty($deprecated_table)) : ?>
+  <div class="<?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
+    <?php foreach ($rows as $row_count => $row): ?>
+      <div <?php if (!empty($row_classes[$row_count])) { print 'class="' . implode(' ', $row_classes[$row_count]) . '"';  } ?>>
+        <?php print $row; ?>
+      </div>
     <?php endforeach; ?>
-  </tbody>
-</table>
-<?php endif; ?>
+  </div>
+  <?php endif; ?>
+
+  <?php if (!empty($deprecated_table)) : ?>
+  <table class="<?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
+    <?php if (!empty($caption)) : ?>
+      <caption><?php print $caption; ?></caption>
+    <?php endif; ?>
+
+    <tbody>
+      <?php foreach ($rows as $row_number => $columns): ?>
+        <tr <?php if (!empty($row_classes[$row_number])) { print 'class="' . implode(' ', $row_classes[$row_number]) .'"';  } ?>>
+          <?php foreach ($columns as $column_number => $item): ?>
+            <td <?php if ($column_classes[$row_number][$column_number]) { print 'class="' . implode(' ', $column_classes[$row_number][$column_number]) .'"';  } ?>>
+              <?php print $item; ?>
+            </td>
+          <?php endforeach; ?>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+  <?php endif; ?>
+</div>
